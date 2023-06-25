1c34591dec53822796de3c2d18a0ee8be38ec737
wyd1216 <49727372+wyd1216@users.noreply.github.com>
2023-06-25 18:24:26 +0800

    Dataset structure and naming conventions, as well as traversal path scripts.

--- script_example/extract.sh
+++
@@ -1,8 +0,0 @@
-python extract.py \
---tmp_keep \
---extract_image_path /media/tx-deepocean/Data/workdir/wyd/ChongFu2HCC/Dataset/liver_cancer_43/IMG \
---extract_mask_path /media/tx-deepocean/Data/workdir/wyd/ChongFu2HCC/Dataset/liver_cancer_43/ANNO \
---extract_image_out /media/tx-deepocean/Data/workdir/wyd/2023/InferScholar/HCC_competition/Dataset/larest_slice_nii/IMG \
---extract_mask_out /media/tx-deepocean/Data/workdir/wyd/2023/InferScholar/HCC_competition/Dataset/larest_slice_nii/ANNO \
---extract_save_format nii \
-
